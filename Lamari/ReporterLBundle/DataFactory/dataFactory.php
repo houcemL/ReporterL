8f630f0d5d60aee51370b3357d58672059910e8c
houcemL <h19_86@yahoo.fr>
2017-04-02 22:06:58 +0100

    Revert "integrate symfony csv decoders"
    This reverts commit 31784d028a1929c7ca8df3e53387ed2aa097098e.

--- Lamari/ReporterLBundle/DataFactory/dataFactory.php
+++ Lamari/ReporterLBundle/DataFactory/dataFactory.php
@@ -11,7 +11,6 @@ namespace Lamari\ReporterLBundle\DataFactory;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Lamari\ReporterLBundle\Encoder\CsvEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class dataFactory
@@ -19,22 +18,12 @@ class dataFactory
 
     protected $serializer;
 
-    public function __construct(Serializer $serializer = null, $encoders = array()) {
-        
-        if (!is_null($serializer) and ( !$this->supportsDecoding('json')
-                or ! $this->supportsDecoding('xml')
-                or ! $this->supportsDecoding('csv'))) {
+    public function __construct(Serializer $sirializer = null, $encoders = array()) {
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $norm = new ObjectNormalizer();
+        $normalizers = array($norm);
 
-            $encoders = array(new XmlEncoder(), new JsonEncoder(), new CsvEncoder());
-
-            $norm = new ObjectNormalizer();
-            $normalizers = array($norm);
-            $this->serializer = new Serializer($normalizers, $encoders);
-            
-        } else {            
-            $this->serializer = $serializer;        
-        }
-        
+        $this->serializer = new Serializer($normalizers, $encoders);
     }
 
     public function serialise($data, $type = 'json') {
